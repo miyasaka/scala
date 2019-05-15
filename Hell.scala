0708fe0d8984e0181863498c663be3c5361fd5bb
Masashi Miyasaka <miyasaka@charlie-s.jp>
2019-05-15 15:19:04 +0900

    branch-01

---
+++ Hell.scala
@@ -0,0 +1,23 @@
+object Hi{
+  def main(args: Array[String]){
+    val lst_0 = List(3,12,7,10,9,8,11,6,13)
+    val lst_1 = 3::5::7::Nil
+    println(lst_0(0))
+    println(lst_1(2))
+    println(lst_0.sorted)
+    println(lst_0.sorted.reverse)
+    println(lst_0.length)
+    for_0(lst_0.sorted)
+  }
+  def for_0(lists: List[Int]) = {
+    println(lists)
+    for (list <- lists) println(list)
+    println("----")
+    for (list <- lists.reverse){
+      println(list)
+    }
+    println("----")
+    println(lists ::: lists.reverse)
+  }
+
+}
