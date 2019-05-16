7bfc86dce1708f67874075623d4424d9a04cd7f2
Masashi Miyasaka <miyasaka@charlie-s.jp>
2019-05-16 16:43:57 +0900

    201905-4

--- Hi_Array.scala
+++ Hi_Array.scala
@@ -1,24 +1,22 @@
 object Hi_Array{
   def main(args: Array[String]){
-    val lst_0 = List(3,12,7,10,9,8,11,6,13)
-    val lst_1 = 3::5::7::Nil
-    println(lst_0(0))
-    println(lst_1(2))
-    println(lst_0.sorted)
-    println(lst_0.sorted.reverse)
-    println(lst_0.length)
-    for_0(lst_0.sorted)
+    val arr_0 = Array(3,12,7,10,9,8,11,6,13)
+    for_0(arr_0.sorted)
+
+    //val lst_1 = 3::5::7::Nil
+    // println(lst_1(2))
   }
-  def for_0(lists: List[Int]) = {
-    println(lists)
-    for (list <- lists) println(list)
-    for (list <- lists.reverse){
-      println(list)
+  def for_0(arrs: Array[Int]) = {
+    println("----for_0() module----")
+    for (arr <- arrs) println(arr)
+    println("----")
+    for (arr <- arrs.reverse){
+      println(arr)
     }
     // List merge
-    println(lists ::: lists.reverse)
-    val lst_merge = lists ::: lists.reverse
-    for (list <- lst_merge.sorted) println(list)
+    //println(lists ::: lists.reverse)
+    //val lst_merge = lists ::: lists.reverse
+    //for (list <- lst_merge.sorted) println(list)
   }
 
 }
